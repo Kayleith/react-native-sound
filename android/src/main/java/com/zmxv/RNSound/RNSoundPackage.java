ca95e904d7054e6a9202779ae492cb12f560128c
Kayleith <brian.john.kang@gmail.com>
2018-01-24 10:17:35 -0800

    Deprecated

--- android/src/main/java/com/zmxv/RNSound/RNSoundPackage.java
+++ android/src/main/java/com/zmxv/RNSound/RNSoundPackage.java
@@ -19,7 +19,6 @@ public class RNSoundPackage implements ReactPackage {
     return modules;
   }
 
-  @Override
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
